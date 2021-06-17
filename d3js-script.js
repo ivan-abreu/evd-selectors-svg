805a37117794030876f7d87ce604db3b7021d52b
ivan-abreu <ivanabreuochoa@gmail.com>
2021-06-17 08:27:49 -0500

    2 files modified

--- d3js-script.js
+++ d3js-script.js
@@ -1,5 +1,5 @@
 d3.svg(
-  'https://raw.githubusercontent.com/ivan-abreu/evd-selectors-svg/master/infografiabase.svg'
+  'https://raw.githubusercontent.com/ivan-abreu/evd-selectors-svg/master/infografia.svg'
 ).then(data => {
   d3.select('#wrapperdv')
     .node()
