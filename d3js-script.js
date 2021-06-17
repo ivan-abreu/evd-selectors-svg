4472b282dc8d030b0bcb0b71e14adfddcbd8c964
ivan-abreu <ivanabreuochoa@gmail.com>
2021-06-17 08:48:51 -0500

    3 files modified

--- d3js-script.js
+++ d3js-script.js
@@ -1,7 +1,8 @@
-d3.svg(
-  'https://raw.githubusercontent.com/ivan-abreu/evd-selectors-svg/master/infografia.svg'
-).then(data => {
-  d3.select('#wrapperdv')
-    .node()
-    .append(data.documentElement);
+var url = 'https://raw.githubusercontent.com/ivan-abreu/evd-selectors-svg/master/infografia.svg';
+// d3.svg(url).then(data => {
+//   d3.select('#wrapperdv').node().append(data.documentElement);
+// });
+
+d3.svg(url).then( function(data) {
+  d3.select('#wrapperdv').node().append(data.documentElement);
 });
